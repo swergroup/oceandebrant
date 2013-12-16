b3365b1260c547fdaca83466e5c3ce7ad430bd9c
Paolo Tresso <plugins@swergroup.com>
2013-12-16 21:30:33 +0100

    no-no

--- www/wordpress/htdocs/wp-content/plugins/pods/ui/fields/_hidden.php
+++
@@ -1,7 +0,0 @@
-<?php
-$attributes = array();
-$attributes[ 'type' ] = 'hidden';
-$attributes[ 'value' ] = $value;
-$attributes = PodsForm::merge_attributes( $attributes, $name, $form_field_type, $options );
-?>
-<input<?php PodsForm::attributes( $attributes, $name, $form_field_type, $options ); ?> />
--- www/wordpress/htdocs/wp-content/plugins/pods/ui/front/widgets.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo $before_widget;
-
-    if ( !empty( $title ) )
-        echo $before_title . $title . $after_title;
-
-    echo pods_shortcode( $args, ( isset( $content ) ? $content : null ) );
-
-    echo $after_widget;
