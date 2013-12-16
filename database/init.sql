4681bea83f033a382f477083ed7d3c62340bf5fb
Paolo Tresso <plugins@swergroup.com>
2013-12-17 00:16:36 +0100

    database setup

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,5 @@
 # We include default installations of WordPress with this Vagrant setup.
 # In order for that to respond properly, default databases should be
 # available for use.
-CREATE DATABASE IF NOT EXISTS `wordpress_dev`;
-GRANT ALL PRIVILEGES ON `wordpress_dev`.* TO 'wp'@'localhost' IDENTIFIED BY 'password';
+CREATE DATABASE IF NOT EXISTS `wp_oceandebrant`;
+GRANT ALL PRIVILEGES ON `wp_oceandebrant`.* TO 'oceandebrant'@'localhost' IDENTIFIED BY 'oceandebrant';
